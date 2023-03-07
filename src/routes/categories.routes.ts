2c7288c79657ebca252a228e6f9f8b2fe5175e00
Luiz Henrique <luizhjramos@outlook.com>
2023-03-07 07:42:47 -0300

    refactor: abstract data manipulate responsability from route to repository

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,23 +1,15 @@
 import { Router } from "express";
-import { Category } from "../model/Category";
+import { CategoriesRepository } from "../repositories/CategoriesRepository";
 
 const categoriesRoutes = Router();
-
-const categories: Category[] = [];
+const categoriesRepository = new CategoriesRepository();
 
 categoriesRoutes.post("/", (request, response) => {
   const { name, description } = request.body;
 
-  const category = new Category();
-  Object.assign(category, {
-    name,
-    description,
-    created_at: new Date(),
-  });
+  categoriesRepository.create({ name, description });
 
-  categories.push(...categories, category);
-
-  return response.status(201).json({ category });
+  return response.status(201).send();
 });
 
 export { categoriesRoutes };
