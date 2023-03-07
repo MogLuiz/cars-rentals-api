90a9dfe608e7458659820fa0dd1fee38d4c8cfa2
Luiz Henrique <luizhjramos@outlook.com>
2023-03-07 07:05:54 -0300

    feat: categories route

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,9 @@
 import express from "express";
+import { categoriesRoutes } from "./routes/categories.routes";
 
 const app = express();
 
-app.get("/", (request, response) => {
-  return response.json({ message: "Ola" });
-});
+app.use(express.json());
+app.use(categoriesRoutes);
 
 app.listen(3333, () => console.log("Server is running!"));
