d693ac5897b9a2e9c2db1915ad87cac288707afd
Luiz Henrique <luizhjramos@outlook.com>
2023-03-07 07:11:18 -0300

    feat: dynamic category id

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,16 +1,20 @@
 import { Router } from "express";
+import { randomUUID } from "node:crypto";
 
 const categoriesRoutes = Router();
 
 const categories = [];
 
-categoriesRoutes.post("/categories", (request, response) => {
+categoriesRoutes.post("/", (request, response) => {
   const { name, description } = request.body;
 
-  categories.push({
+  const category = {
+    id: randomUUID(),
     name,
     description,
-  });
+  };
+
+  categories.push(category);
 
   return response.status(201).send();
 });
