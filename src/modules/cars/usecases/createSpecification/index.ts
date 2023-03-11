f1cde7b3668b1b31439d9eae6941800e7b69e9b1
Luiz Henrique <luizhjramos@outlook.com>
2023-03-11 17:06:57 -0300

    refactor: Make sure the Specifications repository has only one instance with the singleton pattern

--- src/modules/cars/usecases/createSpecification/index.ts
+++ src/modules/cars/usecases/createSpecification/index.ts
@@ -2,6 +2,6 @@ import { CreateSpecificationUseCase } from './CreateSpecificationUseCase';
 import { CreateSpecificationController } from "./CreateSpecificationController";
 import { SpecificationsRepository } from './../../repositories/SpecificationsRepository';
 
-const specificationsRepository = new SpecificationsRepository()
+const specificationsRepository = SpecificationsRepository.getInstance()
 const createSpecificationUseCase = new CreateSpecificationUseCase(specificationsRepository)
 export const createSpecificationController = new CreateSpecificationController(createSpecificationUseCase)
