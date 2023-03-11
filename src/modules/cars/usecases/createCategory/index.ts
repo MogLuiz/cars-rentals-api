bcb7d53d8ee3a1154c72de1d5a0198d14073897a
Luiz Henrique <luizhjramos@outlook.com>
2023-03-10 21:10:29 -0300

    refactor: abstract category post route logic to useCases layer

--- src/modules/cars/usecases/createCategory/index.ts
+++ src/modules/cars/usecases/createCategory/index.ts
@@ -1,2 +1,10 @@
+import { CategoriesRepository } from '../../repositories'
+import { CreateCategoryController } from './CreateCategoryControler'
+import { CreateCategoryUseCase } from './CreateCategoryUseCase'
+
 export * from './CreateCategoryUseCase'
-// export * from './CreateCategoryControler'
+export * from './CreateCategoryControler'
+
+const categoriesRepository = new CategoriesRepository()
+const createCategoryUseCase = new CreateCategoryUseCase(categoriesRepository)
+export const createCategoryController = new CreateCategoryController(createCategoryUseCase)
--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,21 +1,14 @@
 import { Router } from "express";
 import { CategoriesRepository } from "../modules/cars/repositories";
-import { CreateCategoryUseCase } from "../modules/cars/usecases/createCategory";
+import { createCategoryController } from "../modules/cars/usecases/createCategory";
 
 export const categoriesRoutes = Router();
 const categoriesRepository = new CategoriesRepository();
 
 categoriesRoutes.post("/", (request, response) => {
-  const { name, description } = request.body;
-
-  const createCategoryUseCase = new CreateCategoryUseCase(categoriesRepository);
-  createCategoryUseCase.execute({ name, description });
-
-  return response.status(201).send();
+  return createCategoryController.handle(request, response);
 });
 
 categoriesRoutes.get("/", (request, response) => {
   return response.status(201).json(categoriesRepository.list());
 });
-
-
