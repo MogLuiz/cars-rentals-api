810ed2e23bc12f9881e9bd99852e059a426f6f74
Luiz Henrique <luizhjramos@outlook.com>
2023-03-12 07:51:09 -0300

    feat: ImportCategoryController layer

---
+++ src/modules/cars/usecases/importCategory/ImportCategoryController.ts
@@ -0,0 +1,12 @@
+import { ImportCategoryUseCase } from "./ImportCategoryUseCase";
+import { Request, Response } from "express";
+
+export class ImportCategoryController {
+  constructor(private importCategoryUseCase: ImportCategoryUseCase) {}
+
+  handle(request: Request, response: Response): Response {
+    const { file } = request;
+    this.importCategoryUseCase.execute(file);
+    return response.send();
+  }
+}
