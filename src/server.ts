c1f3063fdf46cac2fd44efa9ff55f1aec0fc90b7
Luiz Henrique <luizhjramos@outlook.com>
2023-03-13 05:26:02 -0300

    chore: swagger setup config

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,14 @@
 import express from "express";
 import { routes } from "./routes";
 
+import swaggerUi from "swagger-ui-express";
+import swaggerSetupFile from "./swagger.json"
+
 const app = express();
 
 app.use(express.json());
 app.use(routes);
+
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerSetupFile))
 
 app.listen(3333, () => console.log("Server is running!"));
