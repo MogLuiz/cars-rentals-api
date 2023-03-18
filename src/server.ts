b75bff530bb287698efa3f82e984fc949e8ecc86
Luiz Henrique <luizhjramos@outlook.com>
2023-03-18 18:51:40 -0300

    chore: typeorm configs

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,8 @@
 import express from "express";
 import { routes } from "./routes";
 
+import "./database"
+
 import swaggerUi from "swagger-ui-express";
 import swaggerSetupFile from "./swagger.json"
 
